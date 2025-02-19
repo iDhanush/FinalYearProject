ea914593c607baeea2426981e0cd0776ea3315e4
iDhanush <idhanushm@gmail.com>
2025-02-20 03:09:05 +0530

    admin panel

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from auth.api import auth_router
 from certification.api import cert_router
+from feedback.api import feedback_router
 from unmask.api import unmask_router
 from uploads.api import upload_router
 from starlette.staticfiles import StaticFiles
@@ -20,10 +21,11 @@ app.include_router(auth_router, prefix="/api")
 app.include_router(upload_router, prefix="/api")
 app.include_router(unmask_router, prefix="/api")
 app.include_router(cert_router, prefix="/api")
+app.include_router(feedback_router, prefix="/api")
 
 
 app.mount("/api/dwd", StaticFiles(directory="assets"), name="assets")
 app.mount("/api/certificate", StaticFiles(directory="certificates"), name="certificates")
-# app.mount("/", StaticFiles(directory="dist"), name="index.html")
+app.mount("/admin", StaticFiles(directory="admin"), name="index.html")
 
 uvicorn.run(app, host="localhost", port=8000)
