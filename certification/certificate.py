c48befc0e8d8c40102f0a13f42f94709bd1dc3d6
iDhanush <idhanushm@gmail.com>
2025-02-20 00:29:38 +0530

    certificate design update

--- certification/certificate.py
+++ certification/certificate.py
@@ -10,24 +10,24 @@ os.makedirs("certificates", exist_ok=True)
 print('loading chrome driver')
 chrome_options = Options()
 chrome_options.add_argument("--start-maximized")  # Start maximized
-chrome_options.add_argument("--disable-gpu")
+# # chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-infobars")
-chrome_options.add_argument("--disable-extensions")
+# # chrome_options.add_argument("--disable-infobars")
+# # chrome_options.add_argument("--disable-extensions")
 
 # Set up WebDriver
 service = Service()  # Replace with your chromedriver path if needed
 driver = webdriver.Chrome(service=service, options=chrome_options)
 print('loaded chrome driver')
 
-def html_parser(real_percentage, fake_percentage, file_hash, issued_for, certificate_uid, date):
+def html_parser(real_percentage, fake_percentage, file_hash, issued_for, collection_id, date):
     return f"""
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>UnMask Certificate</title>
+    <title>Document</title>
 
     <style>
       @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
@@ -40,47 +40,42 @@ def html_parser(real_percentage, fake_percentage, file_hash, issued_for, certifi
         font-family: "Poppins", sans-serif;
         padding: 0;
         margin: 0;
-        background-color: #f5f5f5;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        min-height: 100vh;
       }}
 
       .certi-wrapper {{
-        width: 80%;
-        max-width: 90%;
-        margin: 0px;
-      }}
-
-      .certificate {{
         width: 100%;
-        height:100%;
+        height: 100vh;
+        padding: 20px;
+        display: flex;
+        gap: 10px;
+        justify-content: center;
+        align-items: center;
+      }}
+      .certificate {{
+       width: 1600px;
         background-color: #000;
         box-shadow: var(--shadow);
         display: flex;
-        overflow: hidden;
-        border-radius: 10px;
       }}
 
       .left-bar {{
         min-width: 80px;
+        min-height: 100%;
         background: var(--primary);
       }}
 
       .right-side {{
-        flex: 1;
-        padding: 30px;
+        width: 100%;
+        padding: 24px;
         display: flex;
         flex-direction: column;
-        gap: 15px;
+        gap: 10px;
       }}
 
       .certi-head {{
-        font-size: 36px;
+        font-size: 28px;
         font-weight: 600;
         color: var(--primary);
-        margin: 0;
       }}
 
       .certi-head span {{
@@ -97,29 +92,29 @@ def html_parser(real_percentage, fake_percentage, file_hash, issued_for, certifi
         width: 100%;
         display: flex;
         justify-content: space-between;
-        gap: 30px;
+        gap: 20px;
         margin-top: 20px;
         align-items: center;
       }}
 
       .certi-flex-left {{
+        margin-top: -60px;
         display: flex;
         flex-direction: column;
-        gap: 25px;
+        gap: 20px;
         color: #fff;
-        width: 60%;
+        width: 100%;
       }}
 
       .certi-grp {{
         display: flex;
         flex-direction: column;
-        gap: 8px;
+        gap: 5px;
       }}
 
       .grp-title {{
         font-weight: 300;
         font-size: 14px;
-        color: #aaa;
       }}
 
       .grp-name {{
@@ -129,7 +124,6 @@ def html_parser(real_percentage, fake_percentage, file_hash, issued_for, certifi
 
       .certi-id {{
         font-size: 16px;
-        word-break: break-all;
       }}
 
       .certi-flex-right {{
@@ -137,39 +131,12 @@ def html_parser(real_percentage, fake_percentage, file_hash, issued_for, certifi
         color: #fff;
         flex-direction: column;
         align-items: flex-end;
-        gap: 15px;
+        gap: 10px;
         font-weight: 600;
       }}
 
-      .img-container {{
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin: 30px 0;
-      }}
-
-      .cert-img {{
-        max-width: 60%;
-        height: auto;
-      }}
-
-      .prediction {{
-        color: #007bff;
-        display: flex;
-        flex-direction: column;
-        gap: 25px;
-      }}
-
-      .content {{
-        display: flex;
-        align-items: center;
-        font-size: 28px;
-        font-weight: 500;
-      }}
-
-      .fake {{
-        color: #fd3992;
+      .certi-flex-right img {{
+        margin-right: -25px;
       }}
 
       .qr-sec {{
@@ -177,75 +144,91 @@ def html_parser(real_percentage, fake_percentage, file_hash, issued_for, certifi
         color: #fff;
         font-size: 14px;
         align-items: flex-end;
-        gap: 15px;
-        margin-top: 30px;
+        gap: 10px;
+        margin-top: 20px;
       }}
-
-      .verify {{
-        color: #aaa;
+      .cert-img {{
+        padding-block: 40px;
+      }}
+      .img-container {{
+        width: 100%;
+        display: flex;
+        align-items: center;
+      }}
+      .prediction {{
+        color: #007bff;
+        display: flex;
+        align-items: center;
+        gap: 40px;
+      }}
+      .content {{
+        display: flex;
+        align-items: center;
+        gap: 40px;
+        font-size: 30px;
+      }}
+      .fake {{
+        color: #fd3992;
       }}
     </style>
   </head>
   <body>
-    <div class="certi-wrapper">
-      <div class="certificate">
-        <div class="left-bar"></div>
-        <div class="right-side">
-          <h1 class="certi-head">Un<span>Mask</span></h1>
-          <div class="divider"></div>
+    <div class="certificate">
+      <div class="left-bar"></div>
+      <div class="right-side">
+        <h1 class="certi-head">Un<span>Mask</span></h1>
+        <div class="divider"></div>
+        <div class="img-container">
+          <img
+            src="https://i.ibb.co/MCn5NHL/head.png"
+            width="900px"
+            alt="img"
+            class="cert-img"
+          />
+          <div class="prediction">
+            <div class="content">
+              <div class="real">Real : {real_percentage}%</div>
+            </div>
+            <div class="content">
+              <div class="fake">Fake : {fake_percentage}%</div>
+            </div>
+          </div>
+        </div>
 
-          <div class="img-container">
+        <div class="certi-flex">
+          <div class="certi-flex-left">
+            <div class="certi-grp">
+              <div class="grp-title">Issued for</div>
+              <div class="grp-name">{issued_for}</div>
+              <div class="divider"></div>
+            </div>
+            <div class="certi-grp">
+              <div class="grp-title">File Hash</div>
+              <div class="grp-name certi-id">#{file_hash}</div>
+              <div class="divider"></div>
+            </div>
+            <div class="certi-grp">
+              <div class="grp-title">collection id & Token id</div>
+              <div class="grp-name certi-id">{collection_id}</div>
+              <div class="divider"></div>
+            </div>
+          </div>
+          <div class="certi-flex-right">
+            <div class="certi-date">{date}</div>
             <img
-              src="https://i.ibb.co/MCn5NHL/head.png"
-              alt="Authentication Result"
-              class="cert-img"
+              src="https://i.ibb.co/47CcZvw/Medallionss.png"
+              width="200"
+              alt=""
             />
-            <div class="prediction">
-              <div class="content">
-                <div class="real">Real: {real_percentage}%</div>
-              </div>
-              <div class="content">
-                <div class="fake">Fake: {fake_percentage}%</div>
-              </div>
-            </div>
           </div>
-
-          <div class="certi-flex">
-            <div class="certi-flex-left">
-              <div class="certi-grp">
-                <div class="grp-title">Issued for</div>
-                <div class="grp-name">{issued_for}</div>
-                <div class="divider"></div>
-              </div>
-              <div class="certi-grp">
-                <div class="grp-title">File Hash</div>
-                <div class="grp-name certi-id">#{file_hash}</div>
-                <div class="divider"></div>
-              </div>
-              <div class="certi-grp">
-                <div class="grp-title">Certificate Unique ID</div>
-                <div class="grp-name certi-id">{certificate_uid}</div>
-                <div class="divider"></div>
-              </div>
-            </div>
-            <div class="certi-flex-right">
-              <div class="certi-date">{date}</div>
-              <img
-                src="https://i.ibb.co/47CcZvw/Medallionss.png"
-                width="150"
-                alt="Medallion"
-              />
-            </div>
-          </div>
-
-          <div class="qr-sec">
-            <img
-              src="https://i.ibb.co/7jQhnvM/qr-code-xxl.png"
-              width="70"
-              alt="QR Code"
-            />
-            <div class="verify">Verified by unmask.com</div>
-          </div>
+        </div>
+        <div class="qr-sec">
+          <img
+            src="https://i.ibb.co/7jQhnvM/qr-code-xxl.png"
+            width="80"
+            alt=""
+          />
+          <div class="verify">Verified by unmask.com</div>
         </div>
       </div>
     </div>
@@ -283,14 +266,14 @@ def html_to_image(html_string, uid):
     width, height = img.size
 
     # Define how many pixels to crop from each side
-    left_crop = 300  # pixels to remove from left
-    right_crop = 220  # pixels to remove from right
+    left_crop = 0  # pixels to remove from left
+    right_crop = 610  # pixels to remove from right
 
     # Define the crop boundaries
     left = left_crop
     top = 0
     right = width - right_crop
-    bottom = height
+    bottom = height - 850
 
     # Crop the image
     img = img.crop((left, top, right, bottom))
